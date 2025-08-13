fae28bd6831a38b53da4679d42ac4c65b865ae23
OpenSells <opensellscontact@gmail.com>
2025-08-13 11:55:34 +0100

    refactor: rename streamlit frontend folder

--- streamlit/session_bootstrap.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-from streamlit.cookies_utils import init_cookie_manager_mount
-
-def bootstrap() -> None:
-    """Initialise components that must run before any auth checks."""
-    init_cookie_manager_mount()
-    st.session_state.setdefault("_bootstrapped", True)
